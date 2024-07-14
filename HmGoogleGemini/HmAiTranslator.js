2d0e54b4bac1066c2bcaff3525f1466283f45c44
komiyamma <komiyama0629@gmail.com>
2024-07-14 15:08:15 +0900

    デバッグ情報の除去

--- HmGoogleGemini/HmAiTranslator.js
+++ HmGoogleGemini/HmAiTranslator.js
@@ -1,5 +1,5 @@
 ﻿/*
- * HmAiTranslator v1.0.0.1
+ * HmAiTranslatorToEn v1.0.0.2
  *
  * Copyright (c) 2024 Akitsugu Komiyama
  * under the MIT License
@@ -8,7 +8,6 @@
 function onRequestQuestionText() {
 }
 
-debuginfo(2);
 function onCompleteAnswerText(answer_text) {
 
     begingroupundo();
@@ -16,8 +15,9 @@ function onCompleteAnswerText(answer_text) {
     // カーソル位置が行末なら改行
     // (行選択的なことをしている場合は、翻訳結果は次の行から開始するのが適切なため)
     if (column_wcs() >= linelen_wcs()-1) {
+        insertreturn();
         answer_text = answer_text.replace(/(\n|\r| )+$/, '\n');
-        insert("\n" + answer_text);
+        insert(answer_text);
     }
     else {
         answer_text = answer_text.replace(/(\n|\r| )+$/, '');
