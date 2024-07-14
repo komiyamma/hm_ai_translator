af04ea28b926a2edc7b699746c0f1aa83304b9ee
komiyamma <komiyama0629@gmail.com>
2024-07-14 15:10:29 +0900

    こみっと

--- HmOpenAiGpt/HmAiTranslator.js
+++ HmOpenAiGpt/HmAiTranslator.js
@@ -1,1 +1,46 @@
-G:/repogitory/hm_ai_translator/HmGoogleGemini/HmAiTranslator.js
+﻿/*
+ * HmAiTranslatorToEn v1.0.0.2
+ *
+ * Copyright (c) 2024 Akitsugu Komiyama
+ * under the MIT License
+ */
+
+function onRequestQuestionText() {
+}
+
+function onCompleteAnswerText(answer_text) {
+
+    begingroupundo();
+
+    // カーソル位置が行末なら改行
+    // (行選択的なことをしている場合は、翻訳結果は次の行から開始するのが適切なため)
+    if (column_wcs() >= linelen_wcs()-1) {
+        insertreturn();
+        answer_text = answer_text.replace(/(\n|\r| )+$/, '\n');
+        insert(answer_text);
+    }
+    else {
+        answer_text = answer_text.replace(/(\n|\r| )+$/, '');
+        insert(answer_text);
+    }
+    endgroupundo();
+}
+
+function doMyCustomMain({targetAiMacroPath}) {
+
+    if (hidemaru.getSelectedText() == "") {
+        let dll = loaddll(hidemaruExeDir + "\\HmOutputPane.dll");
+        dll.dllfunc.Output(hidemaru.getCurrentWindowHandle(), "選択テキストがありません\r\n");
+        return;
+    }
+
+    const argobj = {
+        onRequestQuestionText: 1,
+        onCompleteAnswerText: 1
+    };
+
+    hidemaru.setTimeout(
+        () => { hidemaru.postExecMacroFile(`"${targetAiMacroPath}"`, argobj); },
+    0 );
+}
+
