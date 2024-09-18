d75ef2d7c9211a450a612d6c8b2c4be513b1d03e
komiyamma <komiyama0629@gmail.com>
2024-09-18 21:50:31 +0900

    こみっと

--- HmOpenAiGpt/HmAiTranslator.js
+++ HmOpenAiGpt/HmAiTranslator.js
@@ -13,8 +13,13 @@ function onCompleteAnswerText(answer_text) {
     answer_text = answer_text.replace(/(\n|\r| )+$/, '\n');
     let answer_text_array = answer_text.replaceAll("\r", "").split("\n");
 
-    // 空行をフィルタリング
-    answer_text_array = answer_text_array.filter(t => t.length > 0);
+    // 実際に要素が存在する先頭の添え字は？
+    const start = answer_text_array.findIndex(el => el !== "");
+
+    // 実際に要素が存在する最後の添え字は？
+    const end = answer_text_array.length - answer_text_array.findIndex(el => el !== "") - 1;
+
+    answer_text = answer_text.slice(start, end + 1);
 
     // 改行を挟んで再結合
     answer_text = answer_text_array.join("\n");
@@ -22,15 +27,22 @@ function onCompleteAnswerText(answer_text) {
     begingroupundo();
 
     // 行末まで選択することで、カーソルが先頭にある。
-    if (selendx() == 0) {
+    if (selendcolumn() == 0) {
+        // 文中ではなく、文全体の塊を翻訳してほしいのだとみなすので、最後に改行を入れる。
+        answer_text = answer_text.endsWith("\n") ? answer_text : answer_text+"\n";
+
         // 行を翻訳するので最後に改行を入れる
-        insert(answer_text + "\n");
+        insert(answer_text);
     }
 
     // 文頭から文末まで選択することで行選択相当になっている。
-    else if (seltopx() == 0 && selendx() == linelen()) {
-        // 文末に最後改行が無いので、先頭に改行を入れる
-        insert("\n" + answer_text + "\n");
+    else if (seltopcolumn() == 0 && selendcolumn() == linelen2()) {
+
+        // 文中ではなく、文全体の塊を翻訳してほしいのだとみなすので、最後に改行を入れる。
+        answer_text = answer_text.endsWith("\n") ? answer_text : answer_text+"\n";
+
+        // 文末に改行が含まれてないので、先頭に改行を入れる
+        insert("\n" + answer_text);
     }
 
     // 通常の選択
@@ -43,9 +55,18 @@ function onCompleteAnswerText(answer_text) {
 
 function doMyCustomMain({aiMacroPath}) {
 
-    if (hidemaru.getSelectedText() == "") {
+    let writeLineOutputPane = (msg) => {
         let dll = loaddll(hidemaruExeDir + "\\HmOutputPane.dll");
-        dll.dllfunc.Output(hidemaru.getCurrentWindowHandle(), "選択テキストがありません\r\n");
+        dll.dllfunc.Output(hidemaru.getCurrentWindowHandle(), msg + "\r\n");
+    }
+
+    if (hidemaru.getSelectedText() == "") {
+        writeLineOutputPane("選択テキストがありません");
+        return;
+    }
+
+    if (!existfile(aiMacroPath)) {
+        writeLineOutputPane("「" + aiMacroPath + "」というファイルは存在しません");
         return;
     }
 
